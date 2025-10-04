b078d9f5a39cbeb30318226bc8eb7bed3b6f3f16
Zoeloi <eaalmendarez@uamv.edu.ni>
2025-10-04 13:52:41 -0600

    Ejercicio Formulario

---
+++ Ejercicio Formulario/Productos/Formularios/FrmArreglo.cs
@@ -0,0 +1,76 @@
+﻿using Productos.Dao;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Productos.Formularios
+{
+    public partial class FrmArreglo : Form
+    {
+        public FrmArreglo()
+        {
+            InitializeComponent();
+        }
+
+        private void tbEdad_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                agregar();
+            }
+        }
+
+        public void agregar()
+        {
+            try
+            {
+                if (int.TryParse(tbEdad.Text, out int edad) && edad > 0 && edad <= 120) EdadDao.edades[EdadDao.pos++] = edad;
+                else MessageBox.Show("Solo se permiten numero enteros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            } catch (IndexOutOfRangeException) {
+                MessageBox.Show("No se puede agregr mas elementos", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            finally
+            {
+                Utileria();
+            }
+        }
+
+        public void Utileria()
+        {
+            tbEdad.Clear();
+            tbEdad.Focus();
+            mostrarEdades();
+        }
+
+        public void mostrarEdades()
+        {
+            lbEdades.DataSource = null;
+            lbEdades.DataSource = EdadDao.edades;
+            lbEdades.Refresh();
+        }
+
+        private void FrmArreglo_Load(object sender, EventArgs e)
+        {
+            mostrarEdades();
+        }
+        
+      
+        private void tbEdad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void lblPromedio_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ Ejercicio Formulario/Productos/Login.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Productos
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.chkShowPw = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario:";
+            // 
+            // tbUsuario
+            // 
+            this.tbUsuario.Location = new System.Drawing.Point(118, 40);
+            this.tbUsuario.Name = "tbUsuario";
+            this.tbUsuario.Size = new System.Drawing.Size(207, 22);
+            this.tbUsuario.TabIndex = 1;
+            this.tbUsuario.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbUsuario_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contraseña:";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(118, 85);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(207, 22);
+            this.tbPassword.TabIndex = 3;
+            this.tbPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbPassword_KeyDown);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(118, 146);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(207, 44);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Iniciar";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // chkShowPw
+            // 
+            this.chkShowPw.AutoSize = true;
+            this.chkShowPw.Location = new System.Drawing.Point(176, 113);
+            this.chkShowPw.Name = "chkShowPw";
+            this.chkShowPw.Size = new System.Drawing.Size(149, 20);
+            this.chkShowPw.TabIndex = 5;
+            this.chkShowPw.Text = "Mostrar Contraseña.";
+            this.chkShowPw.UseVisualStyleBackColor = true;
+            this.chkShowPw.CheckedChanged += new System.EventHandler(this.chkShowPw_CheckedChanged);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 218);
+            this.Controls.Add(this.chkShowPw);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbUsuario);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.CheckBox chkShowPw;
+    }
+}
